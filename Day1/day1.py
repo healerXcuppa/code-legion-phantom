e2dcc34ae257a95480ad82d4f7498b7a0937815a
healerXcuppa <abdulmalikmohammedsalis555@gmail.com>
2026-03-24 05:25:40 +0000

    Day 1: Variables and Printing with f-strings

---
+++ Day1/day1.py
@@ -0,0 +1,8 @@
+# """Day 1: Hello World - Learning Pythonm"""
+# print("Hello, World!")
+# print("Welcome to Code Legion, Phantom!")
+
+name = "Phantom"
+age = 19
+height = 6.0
+print(f"My name is {name}\nI'm {age} years old\nI'm also {height} tall")
