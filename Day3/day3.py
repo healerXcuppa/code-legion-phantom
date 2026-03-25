a28361fc3cbecfe6167b6117258362fb1feb746c
healerXcuppa <abdulmalikmohammedsalis555@gmail.com>
2026-03-25 21:38:18 +0000

    Updated minor wording errors and also added a new line effect in the line 15 print-out function.

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,6 +1,6 @@
 """Day 3: Variable MManipulation and String Operations"""
 #Variable and string value
-name = "Abdul-Malik Mohammed Salis"
+name = "abdul-malik mohammed salis"
 
 #setting operations
 uppercase = name.upper()    #Changing all text or string values to uppercase.
@@ -12,11 +12,11 @@ count = len(name)   #counting the number of elements in the variable {name} valu
 print(f"\nMy name is {name}.")
 print(f"\nName written in only uppercase is {uppercase}.")
 print(f"\nName in lowercase is {lowercase}.")
-print(f"The title format of the name is {title}.")
+print(f"\nThe title format of the name is {title}.")
 print(f"\nThe length of the characters in the variable name value is {count}.")
 
 #Bonus Question -- Slicing first and last 3 letters
 first = name[0:3]
 last = name[-3:]
 print(f"\nThe first three letters sliced from the name are {first}.")
-print(f"\nThe last thre letters in the name variable are {last}.")
+print(f"\nThe last three letters in the name variable are {last}.")
