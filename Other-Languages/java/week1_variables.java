1316dbbe6e678433e47f8efa19bb307a68972469
healerXcuppa <abdulmalikmohammedsalis555@gmail.com>
2026-04-14 19:54:05 +0000

    Java First Assesment (Variables)

---
+++ Other-Languages/java/week1_variables.java
@@ -0,0 +1,27 @@
+//Java Assignment : Variables
+public class week1_variables{
+    public static void main(String[] args){
+        //Variables Listing
+        //String
+        String name = "Phantom.";
+
+        //Integer
+        int age = 15;
+
+        //Boolean
+        boolean isOwner = true;
+
+        //Double
+        double height = 6.03;
+
+        //Output
+        System.out.println();
+        System.out.println("=========== WEEK 1 : VARIABLES (JAVA) ==========");
+        System.out.println();
+        System.out.println("My name is " + name);
+        System.out.println("I'm " + age + " years old and " + height + " feet tall.");
+        System.out.println("Am i the leader of CODE LEGION and owner of this Repository; " + isOwner);
+        System.out.println();
+        System.out.println("================================================");
+    }
+}
