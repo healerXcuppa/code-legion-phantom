b9b75fcd461fbcac9890dd2dc25379c0460d96e8
healerXcuppa <abdulmalikmohammedsalis555@gmail.com>
2026-04-25 20:39:25 +0000

    Assignment for week1 python tutoring.

--- Other-Languages/python/week1_variables.py
+++ Other-Languages/python/week1_variables.py
@@ -1,4 +1,16 @@
-# # print("Hello, World!")
-# x = int(input("Enter a number: "))
-# y = int(input("Enter another number: "))
-# print(f"both numbers are {x} and {y}")
+"""WEEK 1: LECTURE ASSIGNMENT"""
+
+# Collecting user inputs
+print(f"\n{'-' * 10} PERSONAL INTRODUCTION CARD {'-' * 10}\n")
+first_name = input("What is yout first name? ")
+last_name  = input("What is your last name? ")
+age = int(input("How old are you? "))
+fav_color = input("What is your favorite color? ")
+hometown = input("What is the name of your hometown? ")
+
+#OUTPUT
+print(f"\n{'-' * 10} Your Introduction Card {'-' * 10}\n")
+print(f"Name: {first_name} {last_name}")
+print(f"Age: {age}")
+print(f"Favorite Color: {fav_color}")
+print(f"Hometown: {hometown}")
